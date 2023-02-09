f7ccb01d06d3256267767c01a00ce1e9232b09cd
Niels Hofland DA <33940529+N1ck3nd@users.noreply.github.com>
2023-02-09 13:29:46 +0100

    Delete file

--- src/private_todo/Check-DefaultAdministrator.ps1
+++
@@ -1,7 +0,0 @@
-$Domain = (Get-ADDomain).DNSRoot
-$ADDomainInfo = Get-ADDomain $Domain
-$DomainDC = $ADDomainInfo.PDCEmulator
-$DomainAdminAccountSID = "$($ADDomainInfo.DomainSID)-500"
-$DomainDefaultAdminAccount = Get-ADUser $DomainAdminAccountSID -Server $DomainDC -Properties Name,Enabled,Created,PasswordLastSet,LastLogonDate,ServicePrincipalName,SID
-Write-Output "$Domain Default Domain Administrator Account:"
-$DomainDefaultAdminAccount | Select Name,Enabled,Created,PasswordLastSet,LastLogonDate,ServicePrincipalName | Format-Table -AutoSize
--- src/private_todo/Check-ProtectedUsersGroup.ps1
+++
@@ -1,8 +0,0 @@
-$Domain = (Get-ADDomain).DNSRoot
-$ADDomainInfo = Get-ADDomain $Domain
-$DomainDC = $ADDomainInfo.PDCEmulator
-$ProtectedUsersGroupMembership = Get-ADGroupMember 'Protected Users'  -Server $DomainDC
-Write-Output "Domain Protected Users Group Membership:"
-$ProtectedUsersGroupMembership | Select name,DistinguishedName,objectClass | Format-Table
-Write-Output ""
-Write-Output "Domain Protected Users Group has $($ProtectedUsersGroupMembership.count) members"
