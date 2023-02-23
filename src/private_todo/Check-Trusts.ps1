d0b7b6c92c50664f2022c80be6111f3a8a845abe
N1ck3nd <33940529+N1ck3nd@users.noreply.github.com>
2023-02-23 18:15:26 +0100

    Delete file

--- src/private_todo/Check-Trusts.ps1
+++
@@ -1,6 +0,0 @@
-$Domain = (Get-ADDomain).DNSRoot
-$ADDomainInfo = Get-ADDomain $Domain
-$DomainDC = $ADDomainInfo.PDCEmulator
-$ADTrusts = Get-ADTrust -Filter * -Server $DomainDC
-Write-Host "$Domain Active Directory Trusts:"
-$ADTrusts | Select-Object Source, Target, Direction, IntraForest, SelectiveAuth, SIDFilteringForestAware, SIDFilteringQuarantined | Format-Table -AutoSize
