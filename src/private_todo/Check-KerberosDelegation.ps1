a3c8a2df6637e63bb1c7e66ebcbf5183d9805bcd
N1ck3nd <33940529+N1ck3nd@users.noreply.github.com>
2023-03-21 11:36:01 +0100

    Refactor TODO-Get-KerberosDelegation.ps1

--- src/private_todo/Check-KerberosDelegation.ps1
+++ src/private_todo/Check-KerberosDelegation.ps1
@@ -1,35 +1,47 @@
-$Domain = (Get-ADDomain).DNSRoot
-$ADDomainInfo = Get-ADDomain $Domain
-$DomainDC = $ADDomainInfo.PDCEmulator
-$DomainDN = $ADDomainInfo.DistinguishedName
-$KerberosDelegationArray = @()
-[array]$KerberosDelegationObjects = Get-ADObject -filter { ((UserAccountControl -BAND 0x0080000) -OR (UserAccountControl -BAND 0x1000000) -OR (msDS-AllowedToDelegateTo -like '*') -OR (msDS-AllowedToActOnBehalfOfOtherIdentity -like '*')) -AND (PrimaryGroupID -ne '516') -AND (PrimaryGroupID -ne '521') } -Server $DomainDC -prop Name, ObjectClass, PrimaryGroupID, UserAccountControl, ServicePrincipalName, msDS-AllowedToDelegateTo, msDS-AllowedToActOnBehalfOfOtherIdentity -SearchBase $DomainDN
 
-ForEach ($KerberosDelegationObjectItem in $KerberosDelegationObjects) {
-    IF ($KerberosDelegationObjectItem.UserAccountControl -BAND 0x0080000)
-    { $KerberosDelegationServices = 'All Services' ; $KerberosType = 'Unconstrained' }
-    ELSE
-    { $KerberosDelegationServices = 'Specific Services' ; $KerberosType = 'Constrained' }
 
-    IF ($KerberosDelegationObjectItem.UserAccountControl -BAND 0x1000000)
-    { $KerberosDelegationAllowedProtocols = 'Any (Protocol Transition)' ; $KerberosType = 'Constrained with Protocol Transition' }
-    ELSE
-    { $KerberosDelegationAllowedProtocols = 'Kerberos' }
+$OutputObject = @()
 
-    IF ($KerberosDelegationObjectItem.'msDS-AllowedToActOnBehalfOfOtherIdentity')
-    { $KerberosType = 'Resource-Based Constrained Delegation' }
+$Prop = @('Name', 'ObjectClass', 'PrimaryGroupID', 'UserAccountControl',
+    'ServicePrincipalName', 'msDS-AllowedToDelegateTo', 'msDS-AllowedToActOnBehalfOfOtherIdentity')
 
-    $KerberosDelegationObjectItem | Add-Member -MemberType NoteProperty -Name Domain -Value $Domain -Force
-    $KerberosDelegationObjectItem | Add-Member -MemberType NoteProperty -Name KerberosDelegationServices -Value $KerberosDelegationServices -Force
-    $KerberosDelegationObjectItem | Add-Member -MemberType NoteProperty -Name DelegationType -Value $KerberosType -Force
-    $KerberosDelegationObjectItem | Add-Member -MemberType NoteProperty -Name KerberosDelegationAllowedProtocols -Value $KerberosDelegationAllowedProtocols -Force
+$KRBDelegationObjects = Get-ADObject -filter { ((UserAccountControl -BAND 0x0080000) -OR (UserAccountControl -BAND 0x1000000) -OR
+     (msDS-AllowedToDelegateTo -like '*') -OR (msDS-AllowedToActOnBehalfOfOtherIdentity -like '*'))
+    -AND (PrimaryGroupID -ne '516') -AND (PrimaryGroupID -ne '521') } -Properties $Prop
 
-    [array]$KerberosDelegationArray += $KerberosDelegationObjectItem
+
+foreach ($Object in $KRBDelegationObjects) {
+
+    if ($Object.UserAccountControl -BAND 0x0080000) {
+        $KRBDelegationServices = 'All Services'
+        $KRBType = 'Unconstrained'
+    }
+    else {
+        $KRBDelegationServices = 'Specific Services'
+        $KRBType = 'Constrained'
+    }
+
+    if ($Object.UserAccountControl -BAND 0x1000000) {
+        $KRBDelegationAllowedProtocols = 'Any (Protocol Transition)'
+        $KRBType = 'Constrained with Protocol Transition'
+    }
+    else {
+        $KRBDelegationAllowedProtocols = 'Kerberos'
+    }
+
+    if ($Object.'msDS-AllowedToActOnBehalfOfOtherIdentity') {
+        $KRBType = 'Resource-Based Constrained Delegation'
+    }
+
+    $obj = New-Object System.Object
+
+    $obj | Add-Member -MemberType NoteProperty -Name DistinguishedName -Value $Object.DistinguishedName -Force
+    $obj | Add-Member -MemberType NoteProperty -Name Name -Value $Object.Name -Force
+    $obj | Add-Member -MemberType NoteProperty -Name ServicePrincipalName -Value $Object.ServicePrincipalName -Force
+    $obj | Add-Member -MemberType NoteProperty -Name KerberosDelegationServices -Value $KRBDelegationServices -Force
+    $obj | Add-Member -MemberType NoteProperty -Name DelegationType -Value $KRBType -Force
+    $obj | Add-Member -MemberType NoteProperty -Name KerberosDelegationAllowedProtocols -Value $KRBDelegationAllowedProtocols -Force
+    $OutputObject += $obj
 }
 
-$KerberosDelegationArray | Sort-Object DelegationType | Select-Object DistinguishedName, DelegationType, Name, ServicePrincipalName | Format-List
-
-Write-Output ""
-Write-Output "$Domain Domain Accounts with Kerberos Delegation:"
-$KerberosDelegationArray | Sort-Object DelegationType | Select-Object DistinguishedName, DelegationType, Name, ServicePrincipalName | Format-Table -AutoSize
-Write-Output ""
+$OutputObject
