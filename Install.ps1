5a41df3218e91641b34e5986b5e2755dc301463f
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2025-01-02 14:28:36 +0100

    README Update + URL fix

--- Install.ps1
+++ Install.ps1
@@ -1,6 +1,6 @@
 # Run this in an administrative PowerShell prompt to install the InvokeADCheck PowerShell module:
 #
-# iex (New-Object Net.WebClient).DownloadString("https://github.com/sensepost/InvokeADCheck/raw/master/Install.ps1")
+# iex(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/sensepost/InvokeADCheck/refs/heads/main/Install.ps1")
 
 # Some general variables
 $ModuleName = 'InvokeADCheck'
